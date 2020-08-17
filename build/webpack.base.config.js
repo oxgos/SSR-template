641ea6a62eacdba22a3afe66404813301b95c25a
oxgos <122486975@qq.com>
2020-08-17 23:43:32 +0800

    去除style-loader,解决server-bundle报错ducoment undefined

--- build/webpack.base.config.js
+++ build/webpack.base.config.js
@@ -41,7 +41,7 @@ module.exports = {
       {
         test: /\.less$/,
         use: [{
-          loader: "style-loader" // creates style nodes from JS strings
+          loader: "vue-style-loader" // creates style nodes from JS strings
         }, {
           loader: "css-loader" // translates CSS into CommonJS
         }, {
